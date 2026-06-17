19905a583a5042644db96fdb6e763f03f38ef539
Pimika Roy <pimikaroy2@gmail.com>
2026-06-17 21:10:23 +0530

    Adjust margin-top for social icons

--- .ipynb_checkpoints/portfolio-checkpoint.py
+++ .ipynb_checkpoints/portfolio-checkpoint.py
@@ -40,7 +40,7 @@ st.markdown("""
     display:flex;
     justify-content:flex-end;
     gap:15px;
-    margin-top:-40px;
+    margin-top:-60px;
     margin-right:20px;
     margin-bottom:20px;
     
